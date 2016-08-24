84df7378aa854395cabe2c99926817a64a0f33c1
guoyichang <15153511628@163.com>
2016-08-24 08:49:35 +0800

    zengjia

---
+++ GitTest/GitTest/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  GitTest
+//
+//  Created by jredu on 16/8/24.
+//  Copyright © 2016年 jereh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ GitTest/GitTest/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  GitTest
+//
+//  Created by jredu on 16/8/24.
+//  Copyright © 2016年 jereh. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
